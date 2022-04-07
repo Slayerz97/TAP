a822e9f18239227a0b49c08de1e2db070a22c7a7
Slayerz97 <luis.carrizales.20isc@tecsanpedro.edu.mx>
2022-04-07 14:30:22 -0500

    version

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RECURSIVIDAD
+{
+    class Program
+    {
+        
+        public static int potencia(int a, int b)
+        {
+            if (b == 0)
+                return 1;
+            else
+                return (a * potencia(a, b - 1));
+
+
+
+
+           static void Main(string[] args)
+           { Console.WriteLine("La potencia es:" + potencia(2, 2));
+                Console.ReadKey();
+
+           }
+
+       
+           
+
+        
+
+
+
+        
+        
+
+             
+
+        }
+    }
+}
